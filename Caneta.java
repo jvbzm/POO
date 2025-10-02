b9de5baac5de95278ac97d9258342489fbf11bb3
jvbzm <victormb1009@gmail.com>
2025-10-01 21:04:27 -0300

    ativade do dia

--- Caneta.java
+++ Caneta.java
@@ -8,28 +8,43 @@ public class Caneta {
     boolean tampado;
 
     void status(){
-        System.out.println("Modelo " +this.modelo);
-        System.out.println("Uma caneta" + this.cor);
-        System.out.println("Ponta" + this.ponta);
-        System.out.println("Carga " +this.carga);
-        System.out.println("Esta tampado?" +this.tampado);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga + "%");
+        System.out.println("Está tampado? " + this.tampado);
     }
     
     void rabiscar(){
-        if(!this.tampado){
-            System.out.println("Esta tampado");
-        }else{
-            System.out.println("A caneta esta rabiscando...");
+        if(this.tampado){
+            System.out.println("Não posso rabiscar, a caneta está tampada!");
+        } else {
+            System.out.println("A caneta está rabiscando...");
         }
     }
 
     void tampar(){
         this.tampado = true;
-
     }
 
     void destampar(){
         this.tampado = false;
+    }
 
+    // Método main dentro da própria classe
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+
+        c1.modelo = "BIC Cristal";
+        c1.cor = "Azul";
+        c1.ponta = 0.7f;
+        c1.carga = 80;
+        c1.tampar();
+
+        c1.status();
+        c1.rabiscar();
+
+        c1.destampar();
+        c1.rabiscar();
     }
 }
